52e4a8a58b0c22106035e559336c17a6026f2622
ManuJS <emanuelle.menali@hotmail.com>
2017-02-24 17:20:05 -0300

    Initial commit

---
+++ mynewnewlibraryandroid/src/main/java/com/aprendizagem/projeto/mynewnewlibraryandroid/MyClassAndroidLib.java
@@ -0,0 +1,14 @@
+package com.aprendizagem.projeto.mynewnewlibraryandroid;
+
+import com.example.MyClassJava;
+
+/**
+ * Created by emanu on 24/02/2017.
+ */
+
+public class MyClassAndroidLib {
+
+    MyClassJava myClassJavaLib = new MyClassJava();
+    public String joke = myClassJavaLib.tellJoke();
+
+}
---
+++ newlibjava/src/main/java/com/example/MyClassJava.java
@@ -0,0 +1,10 @@
+package com.example;
+
+public class MyClassJava {
+
+    public String tellJoke (){
+        return "alguma piada sem graça em inglês";
+    }
+
+    
+}
