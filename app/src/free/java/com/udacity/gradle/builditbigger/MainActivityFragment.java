caa4b4f6ed3b82da03cd418551bc9e6b953c32dc
ManuJS <emanuelle.menali@hotmail.com>
2017-03-05 03:34:40 -0300

    apareceu o anuncio

--- app/src/free/java/com/udacity/gradle/builditbigger/MainActivityFragment.java
+++ app/src/free/java/com/udacity/gradle/builditbigger/MainActivityFragment.java
@@ -7,9 +7,11 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.ads.AdListener;
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
 import com.google.android.gms.ads.InterstitialAd;
+import com.google.android.gms.ads.MobileAds;
 
 
 /**
@@ -18,6 +20,8 @@ import com.google.android.gms.ads.InterstitialAd;
 public class MainActivityFragment extends Fragment {
 
 
+    private CountDownTimer mCountDownTimer;
+    private InterstitialAd mInterstitialAd;
 
     public MainActivityFragment() {
     }
@@ -27,14 +31,38 @@ public class MainActivityFragment extends Fragment {
                              Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_main, container, false);
 
+        MobileAds.initialize(getActivity(), "ca-app-pub-3940256099942544/1033173712");
+
+        // Create the InterstitialAd and set the adUnitId.
+        mInterstitialAd = new InterstitialAd(getActivity());
+        // Defined in res/values/strings.xml
+        mInterstitialAd.setAdUnitId(getString(R.string.banner_ad_unit_id2));
+
+        mInterstitialAd.setAdListener(new AdListener() {
+            @Override
+            public void onAdClosed() {
+                requestNewInterstitial();
+
+            }
+        });
+
+
+        requestNewInterstitial();
+
         AdView mAdView = (AdView) root.findViewById(R.id.adView);
-        // Create an ad request. Check logcat output for the hashed device ID to
-        // get test ads on a physical device. e.g.
-        // "Use AdRequest.Builder.addTestDevice("ABCDEF012345") to get test ads on this device."
         AdRequest adRequest = new AdRequest.Builder()
                 .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
                 .build();
         mAdView.loadAd(adRequest);
         return root;
     }
+
+    private void requestNewInterstitial() {
+        AdRequest adRequest = new AdRequest.Builder()
+                .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
+                .build();
+
+        mInterstitialAd.loadAd(adRequest);
+    }
+
 }
--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -1,5 +1,6 @@
 package com.udacity.gradle.builditbigger;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
@@ -9,9 +10,13 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.ads.AdListener;
 import com.google.android.gms.ads.AdLoader;
+import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.InterstitialAd;
+import com.google.android.gms.ads.MobileAds;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -25,18 +30,50 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button button = (Button) findViewById(R.id.button_joke);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+        MobileAds.initialize(this, "ca-app-pub-3940256099942544/1033173712");
 
-                tellJoke(view);
+        // Create the InterstitialAd and set the adUnitId.
+        mInterstitialAd = new InterstitialAd(this);
+        // Defined in res/values/strings.xml
+        mInterstitialAd.setAdUnitId(getString(R.string.banner_ad_unit_id2));
+
+        mInterstitialAd.setAdListener(new AdListener() {
+            @Override
+            public void onAdClosed() {
+                requestNewInterstitial();
+
             }
         });
 
 
+        requestNewInterstitial();
 
+
+//        final TextView textView = (TextView) findViewById(R.id.instructions_text_view);
+        Button button = (Button) findViewById(R.id.button_joke);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (BuildConfig.FLAVOR == "free") {
+                    if (mInterstitialAd.isLoaded()) {
+                        mInterstitialAd.show();
+                        tellJoke(view);
+                    }
+                }
+                else
+                    tellJoke(view);
+            }
+        });
     }
+
+    private void requestNewInterstitial() {
+        AdRequest adRequest = new AdRequest.Builder()
+                .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
+                .build();
+
+        mInterstitialAd.loadAd(adRequest);
+    }
+
 
 
     @Override
@@ -63,7 +100,6 @@ public class MainActivity extends AppCompatActivity {
 
     public void tellJoke(View view) {
         View parentView = (View) view.getParent();
-
         new EndpointsAsyncTask(this, parentView).execute();
     }
 
